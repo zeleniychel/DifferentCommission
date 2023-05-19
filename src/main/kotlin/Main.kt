2abcedd8111ced52a6ef078606ecbb7b739f222e
Anton Kamnev AN44 <gish18@gmail.com>
2023-05-20 00:40:06 +0300

    add fun limitCh and transaction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main () {
-    println(calcCommission("Maestro", 80000, 500000))
+    transaction("VK Pay", 35000, 100)
 }
 
 fun calcCommission (card: String = "VK Pay", pastTransfer: Int = 0, currentTransfer: Int) = when (card) {
@@ -12,11 +12,24 @@ fun mastercardMaestroCommission (pastTransfer: Int, currentTransfer: Int): Doubl
     val commission = 0.006
     val additionalCommission = 20
     val limit = 75000
-    return if (pastTransfer < limit) 0.0 else (currentTransfer * commission) + additionalCommission
+    return if (pastTransfer + currentTransfer < limit) 0.0 else (currentTransfer * commission) + additionalCommission
 }
 
 fun visaMirCommission (currentTransfer: Int): Double {
     val commission = 0.0075
     val minCommission = 35
     return if (currentTransfer * commission < minCommission) minCommission.toDouble() else currentTransfer * commission
+}
+
+fun limitCheck (card: String, pastTransfer: Int, currentTransfer: Int) = when (card) {
+    "VK Pay" -> (currentTransfer <= 15000 && pastTransfer + currentTransfer <= 40000)
+    else -> (currentTransfer <= 150000 && pastTransfer + currentTransfer <= 600000)
+}
+
+fun transaction (card: String, pastTransfer: Int, currentTransfer: Int) {
+    if (limitCheck(card, pastTransfer, currentTransfer)) {
+        println(calcCommission(card, pastTransfer, currentTransfer))
+    } else {
+        println("Превышен лимит по карте")
+    }
 }
